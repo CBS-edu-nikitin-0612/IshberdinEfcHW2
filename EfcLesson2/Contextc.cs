4a8a471d2f8a9a34d778b784da0c5593111a0011
Ilfat <ilfat95@mail.ru>
2022-07-11 22:21:09 +0500

    Добавьте файлы проекта.

---
+++ EfcLesson2/Contextc.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Task0
+{
+    public class Contextc : DbContext
+    {
+        DbSet<City> Cities { get; set; }
+        DbSet<Country> Countries { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=TestDB_001;Trusted_Connection=True;");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Country>().HasMany(c => c.Citys);
+            modelBuilder.Entity<Country>().HasKey(c => c.Id);
+        }
+    }
+}
---
+++ EfcLesson2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Task0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using Contextc db = new Contextc();
+            {
+                Country rus = new Country()
+                {
+                    Name = "Russia",
+                    Citys = new List<City>()
+                    {
+                        new City() { Name = "Moscow" },
+                        new City() { Name = "SPB" }
+                    }
+                };
+                db.Add(rus);
+                db.SaveChanges();
+            }
+        }
+    }
+
+
+    public class Country
+    {
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(20), MinLength(2)]
+        [Required]
+        public string Name { get; set; }
+        public List<City> Citys { get; set; }
+        public Country()
+        {
+            Citys = new List<City>();
+        }
+    }
+
+    public class City
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Country Country { get; set; }
+    }
+}
